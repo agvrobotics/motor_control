d6bbc9ad234988881daa0f21ce4a43cf6b28d468
sierra-95 <michaelmachohi@gmail.com>
2025-09-16 23:37:09 +0300

    camera cleaning code

--- motor_control/Motion_V2/ros_ws/agv_control/agv_control/teleop_keyboard.py
+++ motor_control/Motion_V2/ros_ws/agv_control/agv_control/teleop_keyboard.py
@@ -48,10 +48,10 @@ class KeyboardTeleop(Node):
 
                 elif key == '[': 
                     linear = self.linear_speed
-                    angular = self.angular_speed * 0.5
+                    angular = self.angular_speed 
                 elif key == ']':
                     linear = self.linear_speed
-                    angular = -self.angular_speed * 0.5
+                    angular = -self.angular_speed
 
                 elif key == 'q':
                     self.stop_robot()
